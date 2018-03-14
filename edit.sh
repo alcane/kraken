d0a1fafb35b7cb9d6dc23ffe8cb3603381e6d2c6
Ubuntu <ubuntu@ip-172-31-39-173.us-east-2.compute.internal>
2018-03-14 22:50:43 +0000

    working on watch editor

--- edit.sh
+++ edit.sh
@@ -12,14 +12,24 @@ debug=0
 while true; do
 
 args=("NEW" "Create a new pair")
+args+=("DEL" "Delete a pair and watches")
+pairs=()
 for pair in $(ls $watchesdir); do
-	args+=($pair "")
+	pairs+=($pair "")
 done
-s=$(whiptail --title "Select currency pair" --menu "Currency pair:" 20 60 10 "${args[@]}" 3>&1 1>&2 2>&3)
-if [ -z $s ]; then
+s=$(whiptail --title "Select currency pair" --menu "Select a currency pair" 20 60 10 "${args[@]}" "${pairs[@]}" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+	exit
+fi
+if [ -z "$s" ]; then
 	exit 0
 fi
-if [[ $s == "NEW" ]]; then
+if [[ $s == "DEL" ]]; then
+	s=$(whiptail --title "Delete currency pair" --menu "Delete a currency pair" 20 60 10 "${pairs[@]}" 3>&1 1>&2 2>&3)
+	rm -rf $watchesdir/$s
+	continue;
+elif [[ $s == "NEW" ]]; then
 	s=$(whiptail --title "Enter currency pair" --inputbox "Pair:" 8 60 3>&1 1>&2 2>&3)
 	s=$(echo $s | tr '[:lower:]' '[:upper:]')
 	if [ -d $watchesdir/$s ]; then
@@ -35,36 +45,44 @@ args=("NEW" "Create a new watch")
 for watch in $(ls $watchesdir/$s); do
         args+=($watch "")
 done
-s=$(whiptail --title "Select watch to change" --menu "Select watch to change" 20 60 10 "${args[@]}" 3>&1 1>&2 2>&3)
-if [ -z $s ]; then
-        continue
+txt="Currency pair: $currencypair\n"
+txt=$txt"Select watch to change"
+s=$(whiptail --title "Select watch to change" --menu "$txt" 20 60 10 "${args[@]}" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+	exit
+fi
+if [ -z "$s" ]; then
+        exit
 fi
 if [[ $s == "NEW" ]]; then
-        s=$(whiptail --title "Enter new watch" --inputbox "Enter new watch trigger, example 'gt6350' or 'lt7980'" 8 60 3>&1 1>&2 2>&3)
+	txt="Currency pair: $currencypair\n"
+	txt=$txt"Enter new watch trigger, example 'gt6350' or 'lt7980'"
+        s=$(whiptail --title "Enter new watch" --inputbox "$txt" 8 60 3>&1 1>&2 2>&3)
         s=$(echo $s | tr '[:upper:]' '[:lower:]')
         if [ -e $watchesdir/$currencypair/$s ]; then
                 whiptail --msgbox "Watch already exist" 8 60 3>&1 1>&2 2>&3
                 continue
         fi
-	wn=$s
-        r=$(whiptail --title "New rule" --inputbox "Enter watch rule, example '+100' or '-75'" 8 60 3>&1 1>&2 2>&3)
-	echo "$r" > $watchesdir/$currencypair/$s
-        whiptail --msgbox "Watch created: '$s' with rule '$r'" 8 60 3>&1 1>&2 2>&3
+	touch $watchesdir/$currencypair/$s
+        whiptail --msgbox "Watch created: '$s'" 8 60 3>&1 1>&2 2>&3
 fi
 wn=$s
-rule=$(cat $watchesdir/$currencypair/$wn)
+rule=$(cat $watchesdir/$currencypair/$wn | sed 's/\s//g')
 
-txt="Enter the rule for this watch."
-txt=$txt" It can be left empty for a simple watch."
-txt=$txt" Enter DELETE in capital letters to delete this watch"
-s=$(whiptail --title "Edit watch" --inputbox "$txt" 9 60 "$rule" 3>&1 1>&2 2>&3)
-if [ -z $s ]; then
-        continue
+txt="Currency pair: $currencypair\nWatch: $wn\n"
+txt=$txt"Enter the rule for this watch.\n"
+txt=$txt"It can be left empty for a simple watch.\n"
+txt=$txt"Enter DELETE in capital letters to delete this watch."
+s=$(whiptail --title "Edit watch" --inputbox "$txt" 11 60 " $rule" 3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus != 0 ]; then
+	exit
 fi
 if [[ $s == "DELETE" ]]; then
 	rm $watchesdir/$currencypair/$wn
 	continue;
 fi
-echo "$s" > $watchesdir/$currencypair/$wn
+echo "$s" | sed 's/\s//g' > $watchesdir/$currencypair/$wn
 
 done
